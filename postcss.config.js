f9009f2495db7ce04093ba274b36732997700d7b
HighError <hierrorua@gmail.com>
2024-01-18 13:23:31 +0200

    fix: 🐛 Repaired postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {},
-  reactStrictMode: false,
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
-export default withPWA(nextConfig);
